91c8b5be4c53a691fdaafb9a1d8ad7d8ae2028a2
Karthikroyal678 <158208149+Karthikroyal678@users.noreply.github.com>
2024-06-29 02:14:41 +0530

    karthik

---
+++ banner/LeftBanner.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import Media from './Media';
+
+const LeftBanner = () => {
+    const [text] = useTypewriter({
+      words: ["Full Stack Developer.", "DATA SCIENTIST."],
+      loop: true,
+      typeSpeed: 20,
+      deleteSpeed: 10,
+      delaySpeed: 2000,
+    });
+  return (
+    <div className="w-full lgl:w-1/2 flex flex-col gap-20">
+      <div className="flex flex-col gap-5">
+        <h4 className=" text-lg font-normal">WELCOME TO MY PORTFOLIO</h4>
+        <h1 className="text-6xl font-bold text-white">
+          Hi, I'm <span className="text-designColor capitalize">KARTHIK</span>
+        </h1>
+        <h2 className="text-4xl font-bold text-white">
+          a <span>{text}</span>
+          <Cursor
+            cursorBlinking="false"
+            cursorStyle="|"
+            cursorColor="#ff014f"
+          />
+        </h2>
+        <p className="text-base font-bodyFont leading-6 tracking-wide">
+        data scientist is one combines strong technical skills with a deep understanding of business needs and communicates
+        </p>
+      </div>
+     {/* Media */}
+     <Media />
+    </div>
+  );
+}
+
+export default LeftBanner
